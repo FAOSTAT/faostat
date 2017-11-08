b0a358d3e7e15c7022766c1dcfc9f0ee00307559
Solina <daniele.olivotti@fao.org>
2017-11-08 08:56:19 +0100

    modify error translate fr

--- ui/submodules/faostat-ui-download-selector/nls/fr/translate.js
+++ ui/submodules/faostat-ui-download-selector/nls/fr/translate.js
@@ -1,7 +1,7 @@
 /*global define*/
 define({
     select_all: 'Tout Sélectionner',
-    clear_all: 'Tout Sfface',
+    clear_all: 'Tout effacer',
     filter_results: 'Filtrer les résultats',
     filter_results_eg: 'Filtrer les résultats par ex.',
     please_make_one_selection: '*Prière de faire une sélection',
