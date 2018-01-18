cf877dda841c99b4298a7c2eb9085ec954df5e9a
Solina <daniele.olivotti@fao.org>
2018-01-18 11:18:29 +0100

    modify Visualize ET

--- ui/config/browse_by_domain/prod/views/ET.js
+++ ui/config/browse_by_domain/prod/views/ET.js
@@ -108,9 +108,9 @@ define([
                         // temp[late to be applied to the config.template for the custom object
                         template: {
                             title: {
-                                en: "Mean Temperature Change",
-                                fr: "Variation moyenne de température",
-                                es: "Variación media de temperatura "
+                                en: "Mean Temperature Change - {{year}}",
+                                fr: "Variation moyenne de température - {{year}}",
+                                es: "Variación media de temperatura  - {{year}}"
                             },
                            // subtitle: "{{#isMultipleYears year aggregation}}{{/isMultipleYears}}{{year}}"
                         }
@@ -130,7 +130,7 @@ define([
                     allowedFilter: ['item','year'],
                     deniedTemplateFilter: [],
                     filter: {
-                        year:'2016',
+                        year:'2017',
                         area: ["5000>", "351"],
                        // area: ["5000", "5100", "5200", "5300", "5400", "5500"],
                         //"group_by": 'year',
@@ -152,7 +152,7 @@ define([
                                 fr: "Variation moyenne de température de {{item}}",
                                 es: "Variación media de temperatura de {{item}}"
                             },
-                            subtitle: "1961 – 2016"
+                            subtitle: "1961 – 2017"
                         }
                     },
 
@@ -174,7 +174,7 @@ define([
                     allowedFilter: ['item','area'],
                     deniedOnLoadFilter: ['area'],
                     filter: {
-                        year:['1961','1962','1963','1964','1965','1966','1967','1968','1969','1970','1971','1972','1973','1974','1975','1976','1977','1978','1979','1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016'],
+                        year:['1961','1962','1963','1964','1965','1966','1967','1968','1969','1970','1971','1972','1973','1974','1975','1976','1977','1978','1979','1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017'],
                         area: ['5000', '5848', '5849'],
                         element: ['7271']
                     }
