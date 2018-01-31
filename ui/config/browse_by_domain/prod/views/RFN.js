0442156b659fe631e8bfa2cc21eef0d0709c7487
Solina <daniele.olivotti@fao.org>
2018-01-31 10:51:13 +0100

    Visualize data Fertilizers by Nutrient

---
+++ ui/config/browse_by_domain/prod/views/RFN.js
@@ -0,0 +1,251 @@
+/*global define*/
+define([
+    'config/browse_by_domain/Config'
+],function (C) {
+
+    'use strict';
+
+    var i18n = C.i18n || {};
+
+    return {
+
+
+
+        "filter": {
+
+            defaultFilter: {
+                "domain_code": ["RFN"],
+                "show_lists": false
+            },
+
+            items: [
+                {
+                    "id": "element",
+                    "type": "codelist",
+                    "parameter": "element",
+                    "componentType": {
+                        "class": "col-md-3",
+                        "type": "dropDownList"
+                    },
+                    "config": {
+                        "dimension_id": "element",
+                        "defaultCodes": ["5850"],
+                        "filter": {
+                        }
+                    }
+                },
+                {
+                    // id to be applied on the getData request
+                    "id": "area",
+                    "type": "codelist",
+                    "parameter": "area",
+                    "componentType": {
+                        "class": "col-md-3",
+                        "type": "dropDownList"
+                        //"multiple": true
+                    },
+                    "config": {
+                        "dimension_id": "area",
+                        "defaultCodes": ["5000"],
+                        "filter": {
+                        }
+                    }
+                },
+                {
+                    "id": "year",
+                    "type": "codelist",
+                    "parameter": "year",
+                    "componentType": {
+                        "class": "col-md-2",
+                        "type": "dropDownList-timerange"
+                    },
+                    "config": {
+                        "dimension_id": "years",
+                        "defaultCodes": ['2002'],
+                        "filter": {
+                        }
+                    }
+                },
+                $.extend(true, {}, C.filter.aggregation, {
+                    "componentType": {
+                        "class": "hidden"
+                    },
+                    "defaultCodes": ["AVG"]
+                })
+
+            ]
+        },
+
+        dashboard: {
+
+            //data base filter
+            defaultFilter: {
+                domain_code: ['RFN'],
+                item: ["3102"]
+            },
+
+            items: [
+                {
+                    type: 'map',
+                    class: "col-xs-12",
+
+                    // labels
+                    labels: {
+                        // labels to dynamically substitute the title and subtitle
+                        default: {},
+
+                        // temp[late to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "Nutrient nitrogen N (total), {{element}}",
+                                fr: "Élément nutritif azote N (total), {{element}}",
+                                es: "Nutriente nitrógeno N (total), {{element}}"
+                            },
+                            subtitle: "{{#isMultipleYears year aggregation}}{{/isMultipleYears}}{{year}}"
+                        }
+                    },
+
+                    //height:'250px',
+                    config: {
+                        layer: {
+                            colorramp: "YlOrRd",
+                            intervals: 7
+                        },
+                        template: {
+
+                        }
+                    },
+                    allowedFilter: ['item', 'year', 'element', 'aggregation'],
+                    deniedTemplateFilter: [],
+                    filter: {
+                        // TODO: remove the area (in theory should be automatically detected from the domain dimensions/schema)
+                        area: ["5000>", "351"],
+                        "group_by": 'year',
+                        "order_by": 'area'
+                    }
+                },
+                {
+                    type: 'chart',
+                    class: "col-xs-12",
+
+                    // labels
+                    labels: {
+                        // template to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "Nutrient nitrogen N (total), {{element}}",
+                                fr: "Élément nutritif azote N (total), {{element}}",
+                                es: "Nutriente nitrógeno N (total), {{element}}"
+                            },
+                            subtitle: "{{year}}"
+                        }
+                    },
+
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "timeserie",
+                            xDimensions: 'year',
+                            yDimensions: 'unit',
+                            valueDimensions: 'value',
+                            seriesDimensions: ['area', 'item', 'element']
+                        },
+                        template: {},
+                        creator: {}
+                    },
+                    allowedFilter: ['area', 'year', 'item'],
+                    deniedOnLoadFilter: ['area'],
+                    filter: {
+                        area: ["5000", "5848", "5849"]
+                    }
+                },
+                {
+                    type: 'chart',
+                    class: "col-xs-12",
+
+                    labels: {
+                        template: {
+                            title: {
+                                en: "{{element}}",
+                                fr: "{{element}}",
+                                es: "{{element}}"
+                            },
+                            subtitle: "{{#isMultipleYears year aggregation}}{{/isMultipleYears}}{{year}}"
+                        }
+                    },
+
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "pie",
+                            xDimensions: null,
+                            yDimensions: null,
+                            valueDimensions: 'value',
+                            seriesDimensions: ['area']
+                        },
+                        template: {
+                            height: '250px'
+                        },
+                        creator: {}
+                    },
+                    allowedFilter: ['year', 'item', 'aggregation'],
+                    filter: {
+                        // TODO: remove the area (in theory should be automatically detected from the domain dimensions/schema)
+                        area: ["5100", "5200", "5300", "5400", "5500"],
+                        "group_by": 'year',
+                        "order_by": 'area'
+                    }
+                },
+
+                {
+                    type: 'chart',
+                    class: "col-xs-12",
+
+                    // labels
+                    labels: {
+                        // template to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "Top 10 Element",
+                                fr: "10 émetteurs principaux Éléments",
+                                es: "Los 10 Elementos"
+                            },
+                            subtitle: "{{#isMultipleYears year aggregation}}{{/isMultipleYears}}{{year}}"
+                        }
+                    },
+
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "standard",
+                            xDimensions: ['element'],
+                            yDimensions: 'unit',
+                            valueDimensions: 'value',
+                            seriesDimensions: ['area']
+                        },
+                        template: {
+                            height:'250px'
+                            // default labels to be applied
+                        },
+                        creator: {
+                            chartObj: {
+                                chart: {
+                                    type: "column"
+                                }
+                            }
+                        }
+                    },
+                    allowedFilter: ['year', 'item', 'aggregation'],
+                    deniedTemplateFilter: [],
+                    filter: {
+                        area: ["5000>"],
+                        "group_by": 'year',
+                        "order_by": 'value DESC',
+                        "limit": '10'
+                    }
+                }
+            ]
+        }
+
+    };
+});
