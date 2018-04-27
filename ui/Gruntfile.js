8f107249e4d379b2b6e5de3b4e19698ee9a6b906
Riccardo Mazzoni <Riccardo.Mazzoni@fao.org>
2018-04-27 17:48:05 +0200

    Testing.

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -269,7 +269,7 @@ module.exports = function (grunt) {
                 },
                 files: [{
                     expand: true,
-                    cwd: 'build/<%= grunt.config.get("mode") %>/',
+                    cwd: 'build/<%= grunt.config.get("mode") %>/ar/',
                     src: [
                         './**'
                     ]
