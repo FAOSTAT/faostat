2d26b20881a1d56c9bc970a7b6b22fc3107d6422
Mario Triani <Mario.Triani@fao.org>
2018-09-17 09:11:28 +0200

    RL Domain update dashboard

--- ui/config/browse_by_domain/prod/views/RL.js
+++ ui/config/browse_by_domain/prod/views/RL.js
@@ -1,6 +1,8 @@
 /*global define*/
-
-define(function () {
+define([
+    'jquery',
+    'config/browse_by_domain/Config'
+],function ($, C) {
 
     'use strict';
 
@@ -15,18 +17,47 @@ define(function () {
 
             items: [
                 {
+                    "id": "item",
+                    "type": "codelist",
+                    // TODO: in theory that should come from the dimensions schema!!
+                    "parameter": "item",
+                    "componentType": {
+                        "class": "col-sm-4",
+                        "type": "dropDownList"
+                    },
+                    "config": {
+                        "dimension_id": "item",
+                        "defaultCodes": ["6610"],
+                        "filter": {}
+                    }
+                },
+                {
+                    "id": "element",
+                    "type": "codelist",
+                    "parameter": "element",
+                    "componentType": {
+                        "class": "col-sm-4",
+                        "type": "dropDownList"
+                    },
+                    "config": {
+                        "dimension_id": "element",
+                        "defaultCodes": ["7209"],
+                        "filter": {}
+                    }
+                },
+                {
                     "id": "area",
                     "type": "codelist",
                     "parameter": "area",
                     "componentType": {
-                        "class": "col-xs-6 col-sm-6 col-md-4",
-                        "type": "dropDownList"
+                        "class": "col-xs-6 col-sm-6 col-md-3",
+                        "type": "dropDownList",
+                        "multiple": false
                     },
                     "config": {
                         "dimension_id": "area",
                         "defaultCodes": ["5000"],
-                        "filter": {
-                        }
+                        "filter": {}
                     }
                 },
                 {
@@ -34,32 +65,22 @@ define(function () {
                     "type": "codelist",
                     "parameter": "year",
                     "componentType": {
-                        "class": "col-xs-4 col-sm-4 col-md-2",
+                        "class": "col-sm-2",
                         "type": "dropDownList-timerange"
                     },
                     "config": {
                         "dimension_id": "year",
-                        "defaultCodes": ["2000"],
+                        "defaultCodes": ['1994','2014'],
                         "filter": {
                         }
                     }
                 },
-                {
-                    "id": "aggregation",
-                    "type": "static",
-                    "parameter": "operator",
+                $.extend(true, {}, C.filter.aggregation, {
                     "componentType": {
-                        "class": "col-md-2",
-                        "type": "dropDownList"
+                        "class": "hidden"
                     },
-                    "config": {
-                        "defaultCodes": ["AVG"],
-                        "data": [
-                            {"code": "AVG", "label": "average", "selected": true},
-                            {"code": "SUM", "label": "sum", "selected": false}
-                        ]
-                    }
-                }
+                    "defaultCodes": ["AVG"]
+                })
             ]
         },
 
@@ -67,113 +88,177 @@ define(function () {
 
             //data base filter
             defaultFilter: {
-                domain_code: ['RL'],
-                element: [5110],
-                item: [
-                    6621,
-                    6650,
-                    6655
-                ]
+                domain_code: ['RL']
             },
 
             items: [
                 {
-                    type: 'chart',
-                    class: "col-md-12",
+                    type: 'map',
+                    class: "col-xs-12",
 
                     // labels
                     labels: {
-                        // template to be applied to the config.template for the custom object
-                        template: {
-                            title: {
-                                en: "Composition of agricultural area {{area}}",
-                                fr: "Composition de la surface agricole {{area}}",
-                                es: "Composición del área agrícola {{area}}"
-                            },
-                            subtitle: "{{year}}"
-                        }
-                    },
-                    config: {
-                        adapter: {
-                            adapterType: 'faostat',
-                            type: "standard",
-                            xDimensions: 'year',
-                            yDimensions: 'unit',
-                            valueDimensions: 'value',
-                            seriesDimensions: ['area', 'item']
-                        },
-                        template: {},
-                        creator: {
-                            chartObj: {
-                                chart: {
-                                    type: 'column'
-                                },
-                                plotOptions: {
-                                    column: {
-                                        stacking: 'normal'
-                                    }
-                                }
-                            }
-                        }
-                    },
-                    allowedFilter: ['area', 'year'],
-                    filter: {
-                        "order_by": 'year'
-                    }
-                },
-                {
-                    type: 'chart',
-                    class: "col-md-12",
 
-                    // labels?
-                    labels: {
                         // template to be applied to the config.template for the custom object
                         template: {
                             title: {
-                                en: "Composition of global agricultural area",
-                                fr: "Composition de la surface agricole mondiale",
-                                es: "Composición del área agrícola mundial"
+                                en: "{{item}} - {{element}} by country (%)",
+                                fr: "{{item}} - {{element}} par pays (%)",
+                                es: "{{item}} - {{element}} por países (%)"
                             },
                             subtitle: "{{#isMultipleYears year aggregation}}{{/isMultipleYears}}{{year}}"
                         }
                     },
+
+                    //height:'250px',
+                    config: {
+                        layer: {},
+                        template: {}
+                    },
+                    allowedFilter: ['item', 'year', 'element', 'aggregation'],
+                    deniedTemplateFilter: [],
+                    filter: {
+                        area: ["5000>", "351"],
+                        "group_by": 'year',
+                        "order_by": 'area'
+                    }
+                },
+                {
+                    type: 'chart',
+                    class: "col-xs-12",
+
+                    // labels?
+                    labels: {
+                        // temp[late to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "{{item}} - {{element}} (%)",
+                                fr: "{{item}} - {{element}} (%)",
+                                es: "{{item}} - {{element}} (%)"
+                            },
+                            subtitle: "{{year}}"
+                        }
+
+                    },
+
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "timeserie",
+                            xDimensions: "year",
+                            yDimensions: "unit",
+                            valueDimensions: 'value',
+                            seriesDimensions: ['area'],
+                            decimalPlaces: 2
+                        },
+                        template: {},
+                        creator: {}
+                    },
+                    allowedFilter: ['year', 'item', 'element','area'],
+                    filter: {
+                        "order_by": 'area, year'
+                    }
+                },
+                {
+                    type: 'chart',
+                    class: "col-xs-12",
+
+                    // labels
+                    labels: {
+
+                        // template to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                "en":"{{item}} - {{element}} (Top 10 Countries)",
+                                "fr":"{{item}} - {{element}} (10 pays principaux)",
+                                "es":"{{item}} - {{element}} (los 10 países principales)"
+                            },
+                            subtitle: "{{#isMultipleYears year aggregation}}{{/isMultipleYears}}{{year}}"
+                        }
+                    },
+
                     config: {
                         adapter: {
                             adapterType: 'faostat',
                             type: "standard",
-                            xDimensions: 'area',
+                            xDimensions: ['area'],
                             yDimensions: 'unit',
                             valueDimensions: 'value',
-                            seriesDimensions: ['item']
+                            seriesDimensions: ['element'],
+                            decimalPlaces: 2
                         },
-                        template: {},
+                        template: {
+                            height:'250px'
+                            // default labels to be applied
+                        },
                         creator: {
                             chartObj: {
                                 chart: {
-                                    type: 'column'
-                                },
-                                plotOptions: {
-                                    column: {
-                                        stacking: 'normal'
-                                    }
+                                    type: "column"
                                 }
                             }
                         }
                     },
-                    allowedFilter: ['year', 'aggregation'],
+                    allowedFilter: ['year', 'item', 'element', 'aggregation'],
+                    deniedTemplateFilter: [],
                     filter: {
-                        area: [
-                            5100,
-                            5200,
-                            5300,
-                            5400,
-                            5500
-                        ],
-                        "group_by": 'year',
-                        "order_by": 'area'
+                        area: ["5000>"],
+                        "group_by": 'year, item',
+                        "order_by": 'value DESC',
+                        "limit": '10'
+                    }
+                },
+                {
+                    type: 'chart',
+                    class: "col-xs-12",
+
+                    // labels
+                    labels: {
+
+                        // template to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                "en":"{{item}} - {{element}} (Bottom 10 Countries)",
+                                "fr":"{{item}} - {{element}} ((Bottom 10 Countries)",
+                                "es":"{{item}} - {{element}} ((Bottom 10 Countries)"
+                            },
+                            subtitle: "{{#isMultipleYears year aggregation}}{{/isMultipleYears}}{{year}}"
+                        }
+                    },
+
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "standard",
+                            xDimensions: ['area'],
+                            yDimensions: 'unit',
+                            valueDimensions: 'value',
+                            seriesDimensions: ['element'],
+                            decimalPlaces: 2
+                        },
+                        template: {
+                            height:'250px'
+                            // default labels to be applied
+                        },
+                        creator: {
+                            chartObj: {
+                                chart: {
+                                    type: "column"
+                                }
+                            }
+                        }
+                    },
+                    allowedFilter: ['year', 'item', 'element', 'aggregation'],
+                    deniedTemplateFilter: [],
+                    filter: {
+                        area: ["5000>"],
+                        "group_by": 'year, item',
+                        "order_by": 'value ASC',
+                        "limit": '10'
                     }
                 }
             ]
         }
+
     };
 });
